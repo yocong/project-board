956baaad95ac5a4d9997a39f29c8f5231c416c4b
yocong <rnfaos77@naver.com>
2023-09-28 15:41:23 +0900

    #39 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId`로 id를 변경하는 내용을
    모든 자바코드에 반영

--- src/main/java/com/fastcampus/projectboard/dto/UserAccountDto.java
+++ src/main/java/com/fastcampus/projectboard/dto/UserAccountDto.java
@@ -5,7 +5,6 @@ import com.fastcampus.projectboard.domain.UserAccount;
 import java.time.LocalDateTime;
 
 public record UserAccountDto(
-        Long id,
         String userId,
         String userPassword,
         String email,
@@ -16,13 +15,12 @@ public record UserAccountDto(
         LocalDateTime modifiedAt,
         String modifiedBy
 ) { // UserAccountDto 객체 생성하는 정적 팩토리 메서드
-    public static UserAccountDto of(Long id, String userId, String userPassword, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
-        return new UserAccountDto(id, userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
+    public static UserAccountDto of(String userId, String userPassword, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new UserAccountDto(userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
     public static UserAccountDto from(UserAccount entity) { //  엔티티를 DTO로 변환 (UserAccount -> UserAccountDto)
         return new UserAccountDto(
-                entity.getId(),
                 entity.getUserId(),
                 entity.getUserPassword(),
                 entity.getEmail(),
--- src/test/java/com/fastcampus/projectboard/service/ArticleCommentServiceTest.java
+++ src/test/java/com/fastcampus/projectboard/service/ArticleCommentServiceTest.java
@@ -148,7 +148,6 @@ class ArticleCommentServiceTest {
 
     private UserAccountDto createUserAccountDto() {
         return UserAccountDto.of(
-                1L,
                 "uno",
                 "password",
                 "uno@mail.com",
