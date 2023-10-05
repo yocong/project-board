9cbac5a1f6b88337bdd9b2838e36003b5457d480
yocong <rnfaos77@naver.com>
2023-10-05 17:29:01 +0900

    #48 - 문제가 된 테스트 코드 발견 및 수정
    이 부분은 진작에 개발 끝나고 테스트를 완료했던 코드인데,
    어느샌가 옛날 버전으로 돌아가 있다.
    정확한 원인은 알 수 없으나, 복구하고 테스트 완료.
    전체 테스트 통과함

--- src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
@@ -1,28 +1,36 @@
 package com.fastcampus.projectboard.controller;
 
-import com.fastcampus.projectboard.config.SecurityConfig;
+import com.fastcampus.projectboard.config.TestSecurityConfig;
+import com.fastcampus.projectboard.service.ArticleService;
+import com.fastcampus.projectboard.service.PaginationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("View 컨트롤러 - 인증")
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(Void.class)
 public class AuthControllerTest {
 
     private final MockMvc mvc;
 
+    @MockBean private ArticleService articleService;
+    @MockBean private PaginationService paginationService;
+
     public AuthControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
+
 
     @DisplayName("[view][GET] 로그인 페이지 - 정상 호출")
     @Test
@@ -32,7 +40,9 @@ public class AuthControllerTest {
         // When & Then
         mvc.perform(get("/login"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+        then(articleService).shouldHaveNoInteractions();
+        then(paginationService).shouldHaveNoInteractions();
     }
+
 }
